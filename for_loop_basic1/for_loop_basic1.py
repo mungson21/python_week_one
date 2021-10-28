9489172334429c95c1a691b81157ba15941c4a36
m ungs <ungson001@live.com>
2021-10-27 20:37:59 -0700

    challenge #2 complete

--- for_loop_basic1/for_loop_basic1.py
+++ for_loop_basic1/for_loop_basic1.py
@@ -1,7 +1,10 @@
 # Basic - Print all integers from 0 to 150.
 
-for x in range(101):
-    print(x)
+# for x in range(101):
+#     print(x)
 
 # Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+
+for x in range(5, 1000, 5):
+    print(x)
 
