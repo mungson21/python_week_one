1ddf9b04350badfefa25070edc71825d6c2e0419
m ungs <ungson001@live.com>
2021-10-28 12:31:45 -0700

    challenge #4 complete

--- for_loop_basic1/for_loop_basic1.py
+++ for_loop_basic1/for_loop_basic1.py
@@ -12,15 +12,24 @@
 # If divisible by 5, print "Coding" instead.
 # If divisible by 10, print "Coding Dojo".
 
-x = 0
-while x <= 100:
-    print(x)
-    x += 1
-    if x % 10 == 0:
-        print("Coding Dojo")
-        x += 1
-        continue
-    if x % 5 == 0:
-        print("Coding")
-        x += 1
-        continue
+# x = 0
+# while x <= 100:
+#     print(x)
+#     x += 1
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#         x += 1
+#         continue
+#     if x % 5 == 0:
+#         print("Coding")
+#         x += 1
+#         continue
+
+# Whoa. That Sucker's Huge - Add odd integers from 0 to 500,000, and print the final sum.
+sum = 0
+for x in range(0, 500000):
+    if x % 2 != 0:
+        sum += x 
+print(sum)
+
+
